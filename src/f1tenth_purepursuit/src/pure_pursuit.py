1a352eb08e8c9c3544ff3a9f2c732edde2c6ebbb
davidxiang101 <davidxiang101@gmail.com>
2023-12-08 14:16:47 -0500

    speed calc

--- src/f1tenth_purepursuit/src/pure_pursuit.py
+++ src/f1tenth_purepursuit/src/pure_pursuit.py
@@ -11,31 +11,39 @@ from geometry_msgs.msg import PolygonStamped
 from geometry_msgs.msg import Point32
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
-from visualization_msgs.msg import Marker 
+from visualization_msgs.msg import Marker
 import tf
 
 # Global variables for storing the path, path resolution, frame ID, and car details
-plan                = []
-path_resolution     = []
-frame_id            = 'map'
+plan = []
+path_resolution = []
+frame_id = "map"
 # car_name            = str(sys.argv[1])
 # trajectory_name     = str(sys.argv[2])
-raceline            = None
+raceline = None
 
 # # global variables with launch file
-trajectory_name = rospy.get_param('~arg1', 'raceline3.csv')
-print('traj: ', trajectory_name)
+trajectory_name = rospy.get_param("~arg1", "raceline3.csv")
+print("traj: ", trajectory_name)
 
-car_name = rospy.get_param('~arg2', 'car_4')
-print('car name: ', car_name)
+car_name = rospy.get_param("~arg2", "car_4")
+print("car name: ", car_name)
 
 
 # Publishers for sending driving commands and visualizing the control polygon
-command_pub         = rospy.Publisher('/car_4/offboard/command'.format(car_name), AckermannDrive, queue_size = 1)
-polygon_pub         = rospy.Publisher('/car_4/purepursuit_control/visualize'.format(car_name), PolygonStamped, queue_size = 1)
-steering_marker_pub = rospy.Publisher('/car_4/steering_angle_marker', Marker, queue_size=1)
-raceline_pub        = rospy.Publisher('/raceline', Path, queue_size=1)
-goal_pub            = rospy.Publisher('/goal', Marker, queue_size=1)
+command_pub = rospy.Publisher(
+    "/car_4/offboard/command".format(car_name), AckermannDrive, queue_size=1
+)
+polygon_pub = rospy.Publisher(
+    "/car_4/purepursuit_control/visualize".format(car_name),
+    PolygonStamped,
+    queue_size=1,
+)
+steering_marker_pub = rospy.Publisher(
+    "/car_4/steering_angle_marker", Marker, queue_size=1
+)
+raceline_pub = rospy.Publisher("/raceline", Path, queue_size=1)
+goal_pub = rospy.Publisher("/goal", Marker, queue_size=1)
 # Global variables for waypoint sequence and current polygon
 global wp_seq
 global curr_polygon
@@ -43,17 +51,25 @@ max_speed = 60.0
 min_speed = 25.0
 prev_speed = 0.0
 
-wp_seq          = 0
+# tune this
+speed_lookahead = 0.5
+
+wp_seq = 0
 control_polygon = PolygonStamped()
+
 
 def construct_path():
     # Function to construct the path from a CSV file
     # TODO: Modify this path to match the folder where the csv file containing the path is located.
-    file_path = os.path.expanduser('~/catkin_ws/src/f1tenth_purepursuit/path/raceline_high_res.csv'.format(trajectory_name))
-    #file_path = os.path.expanduser('~/map_ws/src/raceline.csv'.format(trajectory_name))
+    file_path = os.path.expanduser(
+        "~/catkin_ws/src/f1tenth_purepursuit/path/raceline_high_res.csv".format(
+            trajectory_name
+        )
+    )
+    # file_path = os.path.expanduser('~/map_ws/src/raceline.csv'.format(trajectory_name))
 
     with open(file_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ',')
+        csv_reader = csv.reader(csv_file, delimiter=",")
         for waypoint in csv_reader:
             plan.append(waypoint)
 
@@ -63,35 +79,36 @@ def construct_path():
             plan[index][point] = float(plan[index][point])
 
     for index in range(1, len(plan)):
-         dx = plan[index][0] - plan[index-1][0]
-         dy = plan[index][1] - plan[index-1][1]
-         path_resolution.append(math.sqrt(dx*dx + dy*dy))
+        dx = plan[index][0] - plan[index - 1][0]
+        dy = plan[index][1] - plan[index - 1][1]
+        path_resolution.append(math.sqrt(dx * dx + dy * dy))
 
-    
     raceline_path = Path()
-    raceline_path.header.frame_id = 'map'
+    raceline_path.header.frame_id = "map"
 
-    for index,point in enumerate(plan):
+    for index, point in enumerate(plan):
         waypoint = PoseStamped()
-        waypoint.header.frame_id = 'map'
+        waypoint.header.frame_id = "map"
         waypoint.pose.position.x = point[0]
         waypoint.pose.position.y = point[1]
         raceline_path.poses.append(waypoint)
-    
+
     global raceline
     raceline = raceline_path
     # rate = rospy.Rate(10)
     # while not rospy.is_shutdown():
     # raceline_pub.publish(raceline_path)
     #     rate.sleep()
-    
+
     print(path_resolution)
+
 
 # Steering Range from -100.0 to 100.0
 STEERING_RANGE = 100.0
 
 # vehicle physical parameters
-WHEELBASE_LEN       = 0.325
+WHEELBASE_LEN = 0.325
+
 
 def purepursuit_control_node(data):
     start_time = rospy.get_time()
@@ -110,40 +127,45 @@ def purepursuit_control_node(data):
     odom_x = data.pose.position.x
     odom_y = data.pose.position.y
 
-
     # TODO 1: The reference path is stored in the 'plan' array.
     # Your task is to find the base projection of the car on this reference path.
     # The base projection is defined as the closest point on the reference path to the car's current position.
     # Calculate the index and position of this base projection on the reference path.
-    #print(plan)
+    # print(plan)
     base_proj_idx = 0
     base_proj_dist = sys.maxsize
-    base_proj_pos = (0,0)
+    base_proj_pos = (0, 0)
 
     for idx in range(len(plan)):
-        x,y,z,w = plan[idx]
+        x, y, z, w = plan[idx]
         dist_to_point = math.sqrt(math.pow(odom_x - x, 2) + math.pow(odom_y - y, 2))
         if base_proj_dist > dist_to_point:
             base_proj_dist = dist_to_point
-            base_proj_pos = (x, y) # only x, y
+            base_proj_pos = (x, y)  # only x, y
             base_proj_idx = idx
     pose_x, pose_y = base_proj_pos
     # Calculate heading angle of the car (in radians)
-    heading = tf.transformations.euler_from_quaternion((data.pose.orientation.x,
-                                                        data.pose.orientation.y,
-                                                        data.pose.orientation.z,
-                                                        data.pose.orientation.w))[2]
+    heading = tf.transformations.euler_from_quaternion(
+        (
+            data.pose.orientation.x,
+            data.pose.orientation.y,
+            data.pose.orientation.z,
+            data.pose.orientation.w,
+        )
+    )[2]
     print("heading: ", heading)
-    
-    #print(base_proj_idx)
+
+    # print(base_proj_idx)
     # TODO 2: You need to tune the value of the lookahead_distance
     # lookahead_distance = 1.83
+
+    # dynamic lookahead distance (needs to be tuned and tested)
     base_distance = 1.6
     max_distance = 1.8
     global prev_speed
-    lookahead_distance = base_distance + ((prev_speed / max_speed) * (max_distance - base_distance))
-
-
+    lookahead_distance = base_distance + (
+        (prev_speed / max_speed) * (max_distance - base_distance)
+    )
 
     # TODO 3: Utilizing the base projection found in TODO 1, your next task is to identify the goal or target point for the car.
     # This target point should be determined based on the path and the base projection you have already calculated.
@@ -153,12 +175,12 @@ def purepursuit_control_node(data):
     # Your code here
     curr_lookahead_dist = 0
     goal_idx = base_proj_idx
-    
+
     while curr_lookahead_dist < lookahead_distance:
         curr_lookahead_dist += path_resolution[goal_idx]
         goal_idx += 1
         goal_idx = goal_idx % len(path_resolution)
-    goal_pos = plan[goal_idx] # gives x, y, z, w
+    goal_pos = plan[goal_idx]  # gives x, y, z, w
     target_x, target_y = goal_pos[:2]
     print(goal_idx)
 
@@ -172,14 +194,16 @@ def purepursuit_control_node(data):
     rotated_x = translated_x * math.cos(-heading) - translated_y * math.sin(-heading)
     rotated_y = translated_x * math.sin(-heading) + translated_y * math.cos(-heading)
 
-    #target_x, target_y = rotated_x, rotated_y
+    # target_x, target_y = rotated_x, rotated_y
     y_t = rotated_y
     x_t = rotated_x
-    dist_to_goal = math.sqrt(x_t*x_t + y_t*y_t)
+    dist_to_goal = math.sqrt(x_t * x_t + y_t * y_t)
     alpha = math.asin(y_t / dist_to_goal)
     # print(alpha)
-    steering_angle = math.degrees(math.atan(2*WHEELBASE_LEN*math.sin(alpha)/lookahead_distance))
-    print('steering angle: ',steering_angle)
+    steering_angle = math.degrees(
+        math.atan(2 * WHEELBASE_LEN * math.sin(alpha) / lookahead_distance)
+    )
+    print("steering angle: ", steering_angle)
 
     # TODO 5: Ensure that the calculated steering angle is within the STEERING_RANGE and assign it to command.steering_angle
     # Your code here
@@ -194,14 +218,13 @@ def purepursuit_control_node(data):
         steering_angle = -30
         print("\n\n\n\n\n\n\n\n\nEXCEED TURNING\n\n\n\n\n\n\n")
 
-    
     command.steering_angle = steering_angle * (10.0 / 3.0)
     print("command angle: ", command.steering_angle)
 
     # TODO 6: Implement Dynamic Velocity Scaling instead of a constant speed
     global max_speed
     global min_speed
-    error = (1 - (abs(command.steering_angle) / STEERING_RANGE))
+    error = 1 - (abs(command.steering_angle) / STEERING_RANGE)
     print("error: ", error)
     dynamic_speed = (error) * (max_speed - min_speed) + min_speed
     command.speed = min(max_speed, dynamic_speed)
@@ -218,28 +241,28 @@ def purepursuit_control_node(data):
     # - pose_x, pose_y: Position of the base projection on the reference path
     # - target_x, target_y: Position of the goal/target point4
 
-    # These are set to zero only so that the template code builds. 
+    # These are set to zero only so that the template code builds.
 
-    base_link    = Point32()
+    base_link = Point32()
     nearest_pose = Point32()
     nearest_goal = Point32()
-    base_link.x    = odom_x
-    base_link.y    = odom_y
+    base_link.x = odom_x
+    base_link.y = odom_y
     nearest_pose.x = pose_x
     nearest_pose.y = pose_y
     nearest_goal.x = target_x
     nearest_goal.y = target_y
     control_polygon.header.frame_id = frame_id
-    control_polygon.polygon.points  = [nearest_pose, base_link, nearest_goal]
-    control_polygon.header.seq      = wp_seq
-    control_polygon.header.stamp    = rospy.Time.now()
+    control_polygon.polygon.points = [nearest_pose, base_link, nearest_goal]
+    control_polygon.header.seq = wp_seq
+    control_polygon.header.stamp = rospy.Time.now()
     wp_seq = wp_seq + 1
     polygon_pub.publish(control_polygon)
 
     steering_marker = Marker()
     steering_marker.header.frame_id = frame_id
     steering_marker.header.stamp = rospy.Time.now()
-    steering_marker.ns = 'steering_angle_marker'
+    steering_marker.ns = "steering_angle_marker"
     steering_marker.type = Marker.ARROW
     steering_marker.scale.x = 1.0
     steering_marker.scale.y = 0.1
@@ -249,14 +272,13 @@ def purepursuit_control_node(data):
     steering_marker.color.b = 1.0
     steering_marker.color.a = 1.0
 
-
     steering_marker.pose.position.x = odom_x
     steering_marker.pose.position.y = odom_y
     steering_marker.pose.position.z = 0.0
 
     steering_angle_rads = steering_angle * (3.14159 / 180.0)
     yaw = heading + steering_angle_rads
-    quat = tf.transformations.quaternion_from_euler(0,0,yaw)
+    quat = tf.transformations.quaternion_from_euler(0, 0, yaw)
 
     steering_marker.pose.orientation.x = data.pose.orientation.x
     steering_marker.pose.orientation.y = data.pose.orientation.x
@@ -268,22 +290,24 @@ def purepursuit_control_node(data):
 
     steering_marker_pub.publish(steering_marker)
 
-    print("Elapsed: ", (rospy.get_time()-start_time) * 1000)
+    print("Elapsed: ", (rospy.get_time() - start_time) * 1000)
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     try:
-
-        rospy.init_node('pure_pursuit', anonymous = True)
+        rospy.init_node("pure_pursuit", anonymous=True)
         if not plan:
-            rospy.loginfo('obtaining trajectory')
+            rospy.loginfo("obtaining trajectory")
             construct_path()
 
-        # This node subsribes to the pose estimate provided by the Particle Filter. 
+        # This node subsribes to the pose estimate provided by the Particle Filter.
         # The message type of that pose message is PoseStamped which belongs to the geometry_msgs ROS package.
-        rospy.Subscriber('/car_4/particle_filter/viz/inferred_pose'.format(car_name), PoseStamped, purepursuit_control_node)
+        rospy.Subscriber(
+            "/car_4/particle_filter/viz/inferred_pose".format(car_name),
+            PoseStamped,
+            purepursuit_control_node,
+        )
         rospy.spin()
 
     except rospy.ROSInterruptException:
-
         pass
---
+++ src/f1tenth_purepursuit/src/smooth.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+
+def calculate_curvature(x, y):
+    """Calculate curvature for each point on the path"""
+    dx_dt = np.gradient(x)
+    dy_dt = np.gradient(y)
+    d2x_dt2 = np.gradient(dx_dt)
+    d2y_dt2 = np.gradient(dy_dt)
+    curvature = (
+        np.abs(d2x_dt2 * dy_dt - dx_dt * d2y_dt2) / (dx_dt**2 + dy_dt**2) ** 1.5
+    )
+    return curvature
+
+
+def calculate_differential_curvature(curvature):
+    return np.gradient(curvature)
+
+
+def identify_turn_zones(
+    curvature, differential_curvature, curvature_threshold, diff_curvature_threshold
+):
+    turning_points = []
+    apex_points = []
+    end_turn_points = []
+    in_turn = False
+
+    for i in range(1, len(curvature) - 1):
+        # Detecting the start of a turn
+        if (
+            curvature[i] > curvature_threshold
+            and differential_curvature[i] > diff_curvature_threshold
+            and not in_turn
+        ):
+            turning_points.append(i)
+            in_turn = True
+
+        # Detecting the apex of the turn
+        if (
+            in_turn
+            and differential_curvature[i] < 0
+            and differential_curvature[i - 1] >= 0
+        ):
+            apex_points.append(i)
+
+        # Detecting the end of a turn
+        if (
+            in_turn
+            and curvature[i] < curvature_threshold
+            and differential_curvature[i] < diff_curvature_threshold
+        ):
+            end_turn_points.append(i)
+            in_turn = False
+
+    return turning_points, apex_points, end_turn_points
+
+
+def create_speed_profile(
+    curvature, turning_points, apex_points, min_speed, max_speed, brake_zone_length
+):
+    speed_profile = np.full(len(curvature), max_speed)
+    braking_zones = []
+    acceleration_zones = []
+
+    for turn in turning_points:
+        for i in range(
+            turn - brake_zone_length, turn
+        ):  # Example: start slowing down brake_zone_length points before the turn
+            speed_profile[i] = np.interp(
+                i, [turn - brake_zone_length, turn], [max_speed, min_speed]
+            )
+            braking_zones.append(i)
+
+    for apex in apex_points:
+        for i in range(
+            apex, apex + brake_zone_length
+        ):  # Example: start speeding up brake_zone_length points after the apex
+            if i < len(speed_profile):
+                speed_profile[i] = np.interp(
+                    i, [apex, apex + brake_zone_length], [min_speed, max_speed]
+                )
+                acceleration_zones.append(i)
+
+    return speed_profile, braking_zones, acceleration_zones
+
+
+def smooth_path(x, y, sigma=3):
+    """Smooth the path using a Gaussian filter"""
+    x_smooth = gaussian_filter1d(x, sigma)
+    y_smooth = gaussian_filter1d(y, sigma)
+    return x_smooth, y_smooth
+
+
+def interpolate_path(x, y, num_points=500):
+    """Interpolate to increase the number of points on the path"""
+    t = np.linspace(0, 1, len(x))
+    t_new = np.linspace(0, 1, num_points)
+    f_x = interp1d(t, x, kind="cubic")
+    f_y = interp1d(t, y, kind="cubic")
+    x_interp = f_x(t_new)
+    y_interp = f_y(t_new)
+    return x_interp, y_interp
+
+
+def smooth_and_refine_raceline(csv_file, output_file, sigma=3, num_points=500):
+    # Load raceline data
+    raceline = pd.read_csv(csv_file, header=None, names=["x", "y", "z", "w"])
+    x_original = raceline["x"].values
+    y_original = raceline["y"].values
+
+    # Smooth the raceline
+    x_smooth, y_smooth = smooth_path(x_original, y_original, sigma=sigma)
+
+    # Increase the number of points
+    x_high_res, y_high_res = interpolate_path(x_smooth, y_smooth, num_points=num_points)
+
+    # Creating a DataFrame for the high-resolution smoothed raceline
+    smoothed_raceline = pd.DataFrame(
+        {
+            "x": x_high_res,
+            "y": y_high_res,
+            "z": np.interp(
+                np.linspace(0, 1, num_points),
+                np.linspace(0, 1, len(raceline)),
+                raceline["z"],
+            ),  # Interpolating z values
+            "w": np.interp(
+                np.linspace(0, 1, num_points),
+                np.linspace(0, 1, len(raceline)),
+                raceline["w"],
+            ),  # Interpolating w values
+        }
+    )
+
+    # Assuming you have curvature data
+    curvature = calculate_curvature(x_high_res, y_high_res)
+
+    # Calculate differential curvature
+    differential_curvature = calculate_differential_curvature(curvature)
+
+    # Update turn detection
+    curvature_threshold = 0.01
+    diff_curvature_threshold = 0.001
+    turning_points, apex_points, end_turn_points = identify_turn_zones(
+        curvature, differential_curvature, curvature_threshold, diff_curvature_threshold
+    )
+
+    turning_points, apex_points = identify_turn_zones(curvature, curvature_threshold)
+    speed_profile, braking_zones, acceleration_zones = create_speed_profile(
+        curvature, turning_points, apex_points, 0.0, 1.0
+    )
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.plot(x_original, y_original, "o", label="Original Points")
+    plt.plot(x_high_res, y_high_res, label="High-Resolution Smoothed Raceline")
+
+    # Highlight braking zones
+    for idx in braking_zones:
+        plt.scatter(x_high_res[idx], y_high_res[idx], color="red", s=10)
+
+    # Highlight acceleration zones
+    for idx in acceleration_zones:
+        plt.scatter(x_high_res[idx], y_high_res[idx], color="green", s=10)
+
+    plt.xlabel("X-coordinate")
+    plt.ylabel("Y-coordinate")
+    plt.title("High-Resolution Raceline with Braking and Acceleration Zones")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # Save the high-resolution smoothed raceline to a CSV file
+    smoothed_raceline.to_csv(output_file, header=False, index=False)
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.plot(x_original, y_original, "o", label="Original Points")
+    plt.plot(x_high_res, y_high_res, label="High-Resolution Smoothed Raceline")
+    plt.xlabel("X-coordinate")
+    plt.ylabel("Y-coordinate")
+    plt.title("High-Resolution Raceline Smoothing")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    return x_high_res, y_high_res
+
+
+# Usage
+smooth_and_refine_raceline(
+    "raceline.csv", "raceline_smoothed.csv", sigma=3, num_points=500
+)
